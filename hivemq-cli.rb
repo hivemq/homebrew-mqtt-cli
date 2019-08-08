e38b2f5fc1bee75f2f23b9c65f631c3d0c9eadf2
Till Seeberger <seebergertill@gmail.com>
2019-08-08 13:54:02 +0200

    Update hivemq-cli.rb

--- hivemq-cli.rb
+++ hivemq-cli.rb
@@ -3,8 +3,9 @@ class HivemqCli < Formula
   homepage "https://www.hivemq.com"
   url "https://www.hivemq.com/tests/hivemq-cli-1.0-distribution.zip"
   sha256 "b79ab87a9064ec122e538e06c6d3c3594440221fd52e450a2831aac5e9a31605"
+  depends_on :java => "1.8+"
+  
   def install
-    depends_on :java => "1.8+"
     inreplace "brew/hivemq-cli", "##PREFIX##", "#{prefix}"
     prefix.install "hivemq-cli.jar"
     bin.install "brew/hivemq-cli"
