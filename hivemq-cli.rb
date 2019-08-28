370662dcaaa54d047cccf9ec3faa42fef27d6b8e
Till Seeberger <till.seeberger@hivemq.com>
2019-08-28 16:11:51 +0200

    Update hivemq-cli.rb

--- hivemq-cli.rb
+++ hivemq-cli.rb
@@ -2,7 +2,7 @@ class HivemqCli < Formula
   desc "The HiveMQ CLI Tool"
   homepage "https://www.hivemq.com"
   url "https://www.hivemq.com/tests/hivemq-cli-1.0-distribution.zip"
-  sha256 "a113610f08ed801c77c14cffb89270355eb575ca65a8ba08c1098d32358d57c5"
+  sha256 "c58b7ac1601f9f52c1487440cf9c734ef52fe179dc8389c88acfdbe81e5208d0"
   depends_on :java => "1.8+"
   
   def install
