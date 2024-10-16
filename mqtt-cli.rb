e5664ff1d0d11ccbb8c94c5fc66c9d8d157300ae
runner <runner@fv-az529-43.k5vkklxpw5gudmwuli4an2t0jd.cx.internal.cloudapp.net>
2024-10-16 09:32:58 +0000

    Release version v4.33.0

--- mqtt-cli.rb
+++ mqtt-cli.rb
@@ -2,7 +2,7 @@ class MqttCli < Formula
   desc "MQTT CLI is a tool that provides a feature rich command line interface for connecting, publishing, subscribing, unsubscribing and disconnecting various MQTT clients simultaneously and supports  MQTT 5.0 and MQTT 3.1.1 "
   homepage "https://www.hivemq.com"
   url "https://github.com/hivemq/mqtt-cli/releases/download/v4.33.0/mqtt-cli-4.33.0-brew.zip"
-  sha256 "502d029862e43ebb27175f6d1f73e1eedc63967a82be11c4c2e59b1ef5145f48"
+  sha256 "7187dd5c066c02a4b5544072f16307c0de661a0d9022de71dda40d60a44341d5"
   # depends_on :java => "1.8+"
 
   def install
