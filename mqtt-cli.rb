a0d94d1851351c47eedfe1ccb59d9e2dc44e2d2b
Yonathan Negussie Mengesha <yonathan.neg@gmail.com>
2024-10-16 12:24:01 +0200

    Update mqtt-cli.rb
    Updated sha

--- mqtt-cli.rb
+++ mqtt-cli.rb
@@ -2,7 +2,7 @@ class MqttCli < Formula
   desc "MQTT CLI is a tool that provides a feature rich command line interface for connecting, publishing, subscribing, unsubscribing and disconnecting various MQTT clients simultaneously and supports  MQTT 5.0 and MQTT 3.1.1 "
   homepage "https://www.hivemq.com"
   url "https://github.com/hivemq/mqtt-cli/releases/download/v4.33.0/mqtt-cli-4.33.0-brew.zip"
-  sha256 "4b4584edd18ea7aeb584fe4ba9b268479658723471c10ddddfaf6fafccb6e055"
+  sha256 "502d029862e43ebb27175f6d1f73e1eedc63967a82be11c4c2e59b1ef5145f48"
   # depends_on :java => "1.8+"
 
   def install
