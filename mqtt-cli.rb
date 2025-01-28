fc4606614f7856ca8d81fe69742c1848d5d13c34
runner <runner@fv-az1657-910.uwkttoivbkeurfcrsjvidt5x2b.cx.internal.cloudapp.net>
2025-01-28 12:15:41 +0000

    Release version v4.36.0

--- mqtt-cli.rb
+++ mqtt-cli.rb
@@ -1,13 +1,13 @@
 class MqttCli < Formula
   desc "MQTT CLI is a tool that provides a feature rich command line interface for connecting, publishing, subscribing, unsubscribing and disconnecting various MQTT clients simultaneously and supports  MQTT 5.0 and MQTT 3.1.1 "
   homepage "https://www.hivemq.com"
-  url "https://github.com/hivemq/mqtt-cli/releases/download/v4.35.0/mqtt-cli-4.35.0-brew.zip"
-  sha256 "4442ebd4f6d7db09b899f6cf753d4af816e8bcf4a291f9488d56f5986320edd6"
+  url "https://github.com/hivemq/mqtt-cli/releases/download/v4.36.0/mqtt-cli-4.36.0-brew.zip"
+  sha256 "20576952af2016d9ce6e15ef6a0b3fa87ba13909766f56193bd771c27dccd7fd"
   # depends_on :java => "1.8+"
 
   def install
-    inreplace "brew/mqtt", "##PREFIX##", "#{prefix}/mqtt-cli-4.35.0.jar"
-    prefix.install "brew/mqtt-cli-4.35.0.jar"
+    inreplace "brew/mqtt", "##PREFIX##", "#{prefix}/mqtt-cli-4.36.0.jar"
+    prefix.install "brew/mqtt-cli-4.36.0.jar"
     bin.install "brew/mqtt"
   end
 
