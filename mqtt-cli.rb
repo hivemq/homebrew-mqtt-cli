43e92663e2df6881497bbac3e325b6082aa5cb6d
Yonathan Negussie Mengesha <yonathan.mengesha@hivemq.com>
2025-12-09 13:51:45 +0100

    Revert incorrect digest update

--- mqtt-cli.rb
+++ mqtt-cli.rb
@@ -2,7 +2,7 @@ class MqttCli < Formula
   desc "MQTT CLI is a tool that provides a feature rich command line interface for connecting, publishing, subscribing, unsubscribing and disconnecting various MQTT clients simultaneously and supports  MQTT 5.0 and MQTT 3.1.1 "
   homepage "https://www.hivemq.com"
   url "https://github.com/hivemq/mqtt-cli/releases/download/v4.47.0/mqtt-cli-4.47.0-brew.zip"
-  sha256 "97a655c14249d04e01542613143ff1ebf69c8a1684ce160a96fe8b941695462d"
+  sha256 "6743d881d86297e8a7387c4823eb19b177954a16b44583d5fd2be9a296814a84"
   # depends_on :java => "1.8+"
 
   def install
